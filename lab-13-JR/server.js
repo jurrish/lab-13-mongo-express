381556a76c32632fa68ef1a3ea5d9e641c78ec2f
Frank Iriarte <jurrish@gmail.com>
2016-12-26 13:49:50 -0800

    server up

--- lab-13-JR/server.js
+++ lab-13-JR/server.js
@@ -3,5 +3,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-
 const app = express();
+const PORT = process.env.PORT || 3000;
+const debug = require('debug')('recipe:server');
+
+app.use(morgan('dev'));
+app.use(cors());
+
+const server = module.exports = app.listen(PORT, function(){
+  debug(`server @ ${PORT}`);
+});
+
+server.isRunning = true;
