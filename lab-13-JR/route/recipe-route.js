faa30cfe9cc2e337167539e2db1dcb75f5f3a91a
Frank Iriarte <jurrish@gmail.com>
2016-12-26 20:56:51 -0800

    get not working. dafuqs

--- lab-13-JR/route/recipe-route.js
+++ lab-13-JR/route/recipe-route.js
@@ -12,3 +12,9 @@ recipeRouter.post('/api/recipe', jsonParser, function(req, res, next) {
   .then(recipe => res.json(recipe))
   .catch(next);
 });
+
+recipeRouter.get('/api/recipe/:id', function(req, res, next) {
+  Recipe.findById(req.params.id)
+  .then(recipe => res.json(recipe))
+  .catch(next);
+});
--- lab-13-JR/test/recipe-route-test.js
+++ lab-13-JR/test/recipe-route-test.js
@@ -40,3 +40,40 @@ describe('testing route /api/recipe', function(){
     });
   });
 });
+
+describe('testing route /api/recipe', function(){
+  describe('testing GET requests', function(){
+    before(done => {
+      testRecipe.timestamp = new Date();
+      new Recipe(testRecipe).save()
+      .then(recipe => {
+        this.testRecipe = recipe;
+        done();
+      });
+    });
+    describe('has valid body', function(){
+      after(done => {
+        delete testRecipe.timestamp;
+        if(this.testRecipe){
+          Recipe.remove({}) //removes all recipes from test db
+          //we pass in an empty object because of queries
+          .then(() => done())
+          .catch(done);
+          return;
+        }
+        done();
+      });
+      it('should return a recipe', done => {
+        request.get(`${url}/api/recipe/${this.testRecipe._id}`)
+        .send(testRecipe)
+        .end((err, res) => {
+          if(err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.name).to.equal('pie');
+          this.testRecipe = res.body;
+          done();
+        });
+      });
+    });
+  });
+});
