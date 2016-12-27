0203584cb2b6412db8e50947fc021b65bb0994ca
Frank Iriarte <jurrish@gmail.com>
2016-12-26 21:44:29 -0800

    put routes done - not tested

--- lab-13-JR/route/recipe-route.js
+++ lab-13-JR/route/recipe-route.js
@@ -8,6 +8,7 @@ const recipeRouter = module.exports = new Router();
 
 recipeRouter.post('/api/recipe', jsonParser, function(req, res, next) {
   req.body.timestamp = new Date();
+  console.log(req.body);
   new Recipe(req.body).save()//returns a promise
   .then(recipe => res.json(recipe))
   .catch(next);
@@ -15,6 +16,12 @@ recipeRouter.post('/api/recipe', jsonParser, function(req, res, next) {
 
 recipeRouter.get('/api/recipe/:id', function(req, res, next) {
   Recipe.findById(req.params.id)
+  .then(recipe => res.json(recipe))
+  .catch(next);
+});
+
+recipeRouter.put('/api/recipe/:id', jsonParser, function(req, res, next) {
+  Recipe.findByIdAndUpdate(req.params.id, req.body, {new: true})
   .then(recipe => res.json(recipe))
   .catch(next);
 });
