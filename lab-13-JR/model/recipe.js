5c27948e05164aa412479b2939cb14af75cde572
Frank Iriarte <jurrish@gmail.com>
2016-12-26 17:44:52 -0800

    setting up object schema

---
+++ lab-13-JR/model/recipe.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// const ObjectId = mongoose.Types.ObjectId; Necessary?
+//so we don't have to type out mongoose.Schema over and over again
+//we want to export a model(constructor)
+
+const recipeSchema = Schema({
+  name: {type: String, required: true}, //name property and type string;
+  //name is REQUIRED since we made it required.
+  ingredients: String,
+  // id: ObjectId(),
+});
--- lab-13-JR/server.js
+++ lab-13-JR/server.js
@@ -3,9 +3,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/dev';
 const debug = require('debug')('recipe:server');
+
+//ask about module caching!!!
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 app.use(morgan('dev'));
 app.use(cors());
